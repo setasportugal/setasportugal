ed4b3a908e4aed113dad41761fb703455c0abe87
setasportugal <zedabola04@gmail.com>
2026-08-05 17:31:31 +0100

    Update page.js

--- app/epocas/[id]/page.js
+++ app/epocas/[id]/page.js
@@ -117,7 +117,7 @@ export default function EpocaPage() {
           </Link>
 
           <Link
-            href={`/jornadas/nova?season=${id}`}
+            href={`/jornadas/nova/${id}`}
             style={{
               textDecoration: 'none'
             }}
