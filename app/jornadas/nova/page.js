2c8676643c8700db7702609c60ab91f8dc4af5e1
setasportugal <zedabola04@gmail.com>
2026-08-05 16:31:29 +0100

    Update page.js

--- app/jornadas/nova/page.js
+++ app/jornadas/nova/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
-import { supabase } from '../../../lib/supabase'
+import { supabase } from '../../lib/supabase'
 
 export default function NovaJornadaPage() {
 
