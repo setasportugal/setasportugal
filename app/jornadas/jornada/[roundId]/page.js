c9ffbe03b05adbd84d458935a663a4cf7eb21b5c
setasportugal <zedabola04@gmail.com>
2026-08-06 13:11:25 +0100

    Update page.js

--- app/jornadas/jornada/[roundId]/page.js
+++ app/jornadas/jornada/[roundId]/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
-import { supabase } from '../../../lib/supabase'
+import { supabase } from '../../../../lib/supabase'
 
 export default function JornadaPage() {
 
